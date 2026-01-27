9e4fdd18cd2cd4d91470771a6b51341ee3d42fe1
AngelMiguensVazquez <amigvaz@gmail.com>
2026-01-27 13:52:42 +0100

    autocommit

--- Larabel/database/migrations/2026_01_27_122406_create_actividades_table.php
+++ Larabel/database/migrations/2026_01_27_122406_create_actividades_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('actividades', function (Blueprint $table) {
             $table->id();
-            $table->date("fecha")
+            $table->date("fecha");
+            $table->integer("tipo");
+            $table->string("contenido");
             $table->timestamps();
         });
     }
